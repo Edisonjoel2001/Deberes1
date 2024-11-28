dd3b6f07727479b9a0ff01436f0041b01516a2da
Edisonjoel2001 <edisonjoel031201@gmail.com>
2024-11-28 00:05:13 -0500

    Deber 1

--- Deber.py
+++ Deber.py
@@ -2,12 +2,14 @@
    #Edison Cabezas
      #Tercer Semestre
         #Primero escribirmos un mensaje de Bienvenida a nuestro programa
-print("Programa para calcular si un numero es mayor o menor que 10")
+print("Bienvenido Programa para calcular si un numero es mayor o menor que 10")
 
-'''Empezamos nuestro programa pidiendo al usuario que ingrese un numero 
-Lo cual vamos a escoger el nombre de RESULTADO, para'''
+#Empezamos nuestro programa pidiendo al usuario que ingrese un numero Lo cual vamos a escoger el nombre de RESULTADO, para definirlo como la variable
+
+#Float es para representar un numero flotante o un decimal
 resultado = float(input("Ingrese un numero: "))
 
+# Utilizamos las condiciones If(si), Elif(Sino,si) y else (Sino)
 if resultado > 10:
     print("El número es mayor que 10.")
 elif resultado < 10:
