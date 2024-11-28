74c4abd20dab5448bb83bb7de88a3f4fdaf47f99
Edisonjoel2001 <edisonjoel031201@gmail.com>
2024-11-28 13:24:23 -0500

    Ejercicio #16

---
+++ Ejercicio#16.py
@@ -0,0 +1,16 @@
+#Ejercicio Número 15
+   #Edison Cabezas
+     #Tercer Semestre
+        #Primero escribirmos un mensaje de Bienvenida a nuestro programa
+
+print("Programa que suma numeros del 1 al 10. ")
+
+# Aqui inicializamos con la variable suma
+suma = 0
+
+# Usamos el bucle for para sumar numeros del 1 al 10
+for i in range(1, 11):
+    suma += i  # Sumar el valor de i a la variable suma
+
+# Mostramos el resultado de la variable
+print(f"La suma de los números del 1 al 10 es: {suma}")
