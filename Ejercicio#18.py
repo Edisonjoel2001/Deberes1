3a1573532e0edbcf814f82b6bedd54cb1f85be14
Edisonjoel2001 <edisonjoel031201@gmail.com>
2024-11-28 13:34:50 -0500

    Ejercicio #18

--- Ejercicio#18.py
+++ Ejercicio#18.py
@@ -5,9 +5,10 @@
 
 print("Programa para que imprima todos los numeros pares entre el 1 y el 20. ")
 
-# Usar un bucle for para imprimir los números pares entre 1 y 20
+# Usar un bucle para imprimir los números del 10 al 1 en orden descendente
 
-for i in range(2, 21, 2):  # Comienza en 2, termina en 21 (sin incluirlo), y da pasos de 2
+for i in range(10, 0, -1):  # Comienza en 10, termina en 1 (sin incluir el 0), y da pasos de -1
 
     print(i)
+
 
