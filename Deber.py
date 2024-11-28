a68f09c598a8c9d30b745a23ebc497a0226a086a
Edisonjoel2001 <edisonjoel031201@gmail.com>
2024-11-27 23:55:07 -0500

    Deber 1

---
+++ Deber.py
@@ -0,0 +1,14 @@
+#Deber Numero #1
+#Edison Cabezas
+#
+print("Programa para calcular si un numero es mayor o menor que 10")
+
+resultado = float(input("Ingrese un numero: "))
+
+if resultado > 10:
+    print("El número es mayor que 10.")
+elif resultado < 10:
+    print("El número es menor que 10.")
+else:
+    print("El número es igual a 10.")
+
