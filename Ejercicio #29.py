df7030b486b1b6d1269eff30fde0fbd5847105dd
Edisonjoel2001 <edisonjoel031201@gmail.com>
2024-11-28 14:59:59 -0500

    Ejercicio #29

---
+++ Ejercicio #29.py
@@ -0,0 +1,9 @@
+#Ejercicio Número 29
+   #Edison Cabezas
+     #Tercer Semestre
+        #Primero escribirmos un mensaje de Bienvenida a nuestro programa
+
+print("Imprime los números del 10 al 1 en orden descendente. ")
+
+for i in range(10, 0, -1): #Utilizamos el bucle for y -1 para que sea de forma descendente
+    print(i) #Imprimimos la cadena al revez
